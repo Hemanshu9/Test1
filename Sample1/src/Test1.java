663ecfaf86342864b1aa68883be6fdc6b0fcde7f
0033S1744 <0033S1744@LAPTOP-SJS43K5R>
2022-02-14 14:00:01 +0530

    First commit

---
+++ Sample1/src/Test1.java
@@ -0,0 +1,14 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		        
+		      int num1 = 58, num2 = 15, sum;
+		      sum = num1 + num2;
+
+		      System.out.println("Sum of these numbers: "+sum);
+		   }
+	}
+
+
