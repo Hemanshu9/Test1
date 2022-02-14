1ad95ec2cb6005f6cd90cae2bdd9d5435c33902e
0033S1744 <0033S1744@LAPTOP-SJS43K5R>
2022-02-14 14:09:44 +0530

    changed value

--- Sample1/src/Test1.java
+++ Sample1/src/Test1.java
@@ -4,7 +4,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		        
-		      int num1 = 58, num2 = 155, sum;
+		      int num1 = 59, num2 = 155, sum;
 		      sum = num1 + num2;
 
 		      System.out.println("Sum of these numbers: "+sum);
